4b7e2c099d54299799478e39cf354beb6cd66ba2
MCEpicNation <OfficialGNMCEpicNation@users.noreply.github.com>
2019-06-24 21:52:50 -0400

    Create Coding-Language-Label.js

---
+++ Coding-Language-Label.js
@@ -0,0 +1,6 @@
+// This file is to simply mark it as "Javascrpt."
+// The actual code is in a private respository. :)
+//////////////////////////////////////////////////////////
+//                  Galaxy Network MAB                  //
+//       Copyright MCEpic_Nation & Galaxy Network       //
+//////////////////////////////////////////////////////////
